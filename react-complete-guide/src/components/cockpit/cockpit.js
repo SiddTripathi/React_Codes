bf2161dd6feab24fd46da8769e99a763cdb2a906
Siddharth <siddhart.tripathi@gmail.com>
2020-05-31 21:39:21 +1200

    Adding components/Code Optimization
    Structured the code
    added more components to optimize the code

---
+++ react-complete-guide/src/components/cockpit/cockpit.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import classesmodule from './Cockpit.css'
+const cockpit = (props) => {
+
+    const clases = [];
+    let btnClass = [classesmodule.button];
+    if (props.showPersons === true) {
+        btnClass.push(classesmodule.red)
+    }
+
+    if (props.persons.length <= 2) {
+        clases.push(classesmodule.red);
+    }
+    if (props.persons.length <= 1) {
+        clases.push(classesmodule.bold); //bold is class in app.css
+    }
+
+    return (
+        <div className={classesmodule.Cockpit}>
+            <h1>Hi, I'm a React App</h1>
+            <p className={clases.join(' ')}>This is really working!</p>
+            <button className={btnClass.join(' ')} onClick={props.toggleNameHandler}>Switch Name</button>
+        </div>
+    );
+
+
+}
+
+export default cockpit;
---
+++ react-complete-guide/src/containers/App.js
@@ -0,0 +1,99 @@
+//useState allows us to manage states in class based components.
+
+import React, { Component } from 'react';
+import Persons from '../components/Persons/persons'
+import classesmodule from './App.css';
+import Cockpit from '../components/cockpit/cockpit'
+
+
+
+class App extends Component {
+  state = {
+    persons: [
+      { id: 'P101', name: 'Max', age: 28 },
+      { id: 'P102', name: 'Manu', age: 29 },
+      { id: 'P103', name: 'Stephanie', age: 26 }
+    ],
+    otherState: 'some other value',
+    showPersons: false
+  }
+
+
+  toggleNameHandler = () => {
+
+    const state = this.state.showPersons
+    this.setState({ showPersons: !state }) //state will always be set opposite to current state
+
+  }
+  deletePersonHandler = (personIndex) => {
+    // const persons = this.state.persons --> this is creating a persons reference to original state data. By this 
+    //we are mutating the original data. 
+    const persons = [...this.state.persons];//using spread operator is the appropriate way here. This will convert persons
+    // into an array instead of reference to original state and mutating original data
+    persons.splice(personIndex, 1);
+    this.setState({ persons: persons })
+  }
+  nameChangedHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id
+    })
+
+    const person = {
+      ...this.state.persons[personIndex]
+    }
+
+    person.name = event.target.value
+    const persons = [...this.state.persons];
+    persons[personIndex] = person
+    this.setState({ persons: persons })
+
+  }
+
+  render() {
+
+    let person = null;
+    if (this.state.showPersons === true) {
+      person = (<div>
+        <h1>This is correct</h1>
+        <p>This is rendered through if else cond and JS. Always remeber that react components are Java script. Every thing inside render
+        is executed when components are re-rendered.
+  </p>
+
+      </div>
+      )
+
+    }
+    return (
+
+      <div className={classesmodule.App}>
+
+        {this.state.showPersons ?
+          <div>
+            <Persons
+              persons={this.state.persons}
+              clicked={this.deletePersonHandler}
+              changed={this.nameChangedHandler}
+            ></Persons>
+
+
+          </div> : null}
+        <Cockpit
+          showPersons={this.state.showPersons}
+          persons={this.state.persons}
+          toggleNameHandler={this.toggleNameHandler}
+        >
+          {person}
+        </Cockpit>
+
+      </div>
+
+    );
+
+  }
+}
+
+export default App;
+
+
+
+
